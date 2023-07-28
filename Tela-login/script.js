f3e2adbc0abeb87c966a77aaf09d25f7238529f6
Jhonnee101 <joaobrenogs@gmail.com>
2023-07-28 16:14:52 -0300

    ref
    Criada a pagina de cadastro, falta o script

--- Tela-login/script.js
+++ Tela-login/script.js
@@ -1,3 +1,18 @@
-function entrar(){
-    location.assign('index.html')
+function logar(){
+
+var login = document.getElementById('email').value;
+var senha = document.getElementById('password').value;
+if (login == "joaobreno@gmail.com" && senha == "admin"){
+    alert('Sucesso');
+    location.href = "lista-mercado/lista-mercado.html";
+}else{
+    alert('Usuario ou senha incorretos')
+}
+ 
+
+
+
+
+
+
 }
