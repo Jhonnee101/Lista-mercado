ec89c3e1a1be32c53c1604fd619b6f1a318bab33
Jhonnee101 <joaobrenogs@gmail.com>
2023-07-28 16:46:02 -0300

    ref
    Feito as configuracoes, falta os scripts

--- tela-login/login.js
+++ tela-login/login.js
@@ -2,9 +2,9 @@ function logar(){
 
 var login = document.getElementById('email').value;
 var senha = document.getElementById('password').value;
-if (login == "joaobreno@gmail.com" && senha == "admin"){
+if (login == "joaobreno@gmail.com" && senha == "12345"){
     alert('Sucesso');
-    location.href = "lista-mercado/lista-mercado.html";
+    location.href ="/../mercado/lista.html";
 }else{
     alert('Usuario ou senha incorretos')
 }
